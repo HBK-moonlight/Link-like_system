fe35bae313cde24190f302c82c2af59bd5eae85b
HBK-moonlight <156154388+HBK-moonlight@users.noreply.github.com>
2025-03-02 15:28:54 +0900

    test

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
+import discord
+from discord import message
+from discord.ext import tasks
 import shelve
 import datetime
 from datetime import time, timedelta, timezone
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+  global sc_channel
+  if (str(message.author.id) == '1180377638766907463'):
+    if (message.channel.id == int(sc_channel)):
+      last_msg_desc = message.embeds[0].description
+      print(last_msg_desc)
+    
+
+'''
 with shelve.open('next_wm_time') as shelf_file:
   # 変数を取得する
   day = shelf_file['day']
@@ -26,3 +43,4 @@ with shelve.open('next_fl_time') as shelf_file:
   day_op = datetime.datetime.strptime(day,
                                       '%m/%d') - datetime.timedelta(days=2)
   print(day_op.strftime('%m-%d'))
+'''
